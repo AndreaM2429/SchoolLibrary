4c8ae6621d52febb4903e114eb324fae1ae943b5
Andrea Manuel <andreamanuel2207@gmail.com>
2023-08-28 15:21:53 -0600

    Create Student class that inherits the Person class

---
+++ student.rb
@@ -0,0 +1,17 @@
+require_relative 'person'
+
+class Student < Person
+  def initialize(age, classroom, name = 'Unknown', parent_permission: true)
+    super(age, name, parent_permission)
+    @classroom = classroom
+  end
+
+  def play_hooky
+    '¯\(ツ)/¯'
+  end
+end
+
+student1 = Student.new(15, 2)
+p 'CLASS STUDENT'
+p student1.can_use_services?
+p student1.play_hooky
