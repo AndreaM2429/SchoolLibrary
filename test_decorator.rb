7bbbd0df7a1e903a9cf5e2a804dda24a27ff73b8
Andrea Manuel <andreamanuel2207@gmail.com>
2023-08-28 17:30:30 -0600

    Add a file that check the managed to decorate person

---
+++ test_decorator.rb
@@ -0,0 +1,10 @@
+require_relative 'person'
+require_relative 'capitalize_decorator'
+require_relative 'trimmer_decorator'
+
+person = Person.new(22, 'maximilianus')
+person.correct_name
+capitalized_person = CapitalizeDecorator.new(person)
+puts capitalized_person.correct_name
+capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
+puts capitalized_trimmed_person.correct_name
