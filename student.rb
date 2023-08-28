bb2c0368e7da68dcbe60696ccba0889ea2630a16
Andrea Manuel <andreamanuel2207@gmail.com>
2023-08-28 15:32:02 -0600

    Solve problem in the student and teacher arguments

--- student.rb
+++ student.rb
@@ -2,7 +2,7 @@ require_relative 'person'
 
 class Student < Person
   def initialize(age, classroom, name = 'Unknown', parent_permission: true)
-    super(age, name, parent_permission)
+    super(age, name, parent_permission: parent_permission)
     @classroom = classroom
   end
 
