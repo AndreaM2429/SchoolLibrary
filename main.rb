99a6a57963a710a5e18a8d8e319b82964e7b083a
Andrea Manuel <andreamanuel2207@gmail.com>
2023-09-04 15:19:14 -0600

    Refactor main.rb to request and validate input actions

--- main.rb
+++ main.rb
@@ -1,16 +1,33 @@
 require_relative 'app'
-require_relative 'choose'
 
 class Main
   def initialize
     @app = App.new
+    @menu = {
+      '1' => 'list_all_books', '2' => 'list_all_people',
+      '3' => 'create_a_person', '4' => 'create_a_book',
+      '5' => 'create_a_rental', '6' => 'list_all_rentals_for_person',
+      '7' => 'end_app'
+    }.freeze
+  end
+
+  def menu
+    puts "\nPlease choose an option by enter a number"
+    @menu.each { |key, element| puts "#{key} - #{element.capitalize.gsub '_', ' '}" }
+    print "\nYour option: "
+    action = @menu[gets.chomp]
+    if action
+      action
+    else
+      puts "\n(͠°◡͡°) Invalid option select a valid one\n\n"
+      choose
+    end
   end
 
   def start
     puts "Welcome to School Library App ◦°˚ヽ(•◡•)ノ˚°◦\n\n"
     loop do
-      @choose = Choose.new.choose
-      @app.send(@choose)
+      @app.send(menu)
     end
   end
 end
