269e1957f5d0bf0890f6bb2a5f6f46eae170f99f
Andrea Manuel <andreamanuel2207@gmail.com>
2023-09-04 13:34:58 -0600

    Create a separate class for the method choose

---
+++ choose.rb
@@ -0,0 +1,23 @@
+class choose
+  def initialize
+    MENU = {
+    '1' => 'list_all_books', '2' => 'list_all_people',
+    '3' => 'create_a_person', '4' => 'create_a_book',
+    '5' => 'create_a_rental', '6' => 'list_all_rentals_for_person',
+    '7' => 'end_app'
+    }.freeze
+  end
+
+  def choose
+    puts "\nPlease choose an option by enter a number"
+    MENU.each { |key, element| puts "#{key} - #{element.capitalize.gsub '_', ' '}" }
+    print "\nYour option: "
+    action = MENU[gets.chomp]
+    if action
+      action
+    else
+      puts "\n(͠°◡͡°) Invalid option select a valid one\n\n"
+      choose
+    end
+  end
+end
--- main.rb
+++ main.rb
@@ -1,25 +1,5 @@
 require_relative 'app'
 
-MENU = {
-  '1' => 'list_all_books', '2' => 'list_all_people',
-  '3' => 'create_a_person', '4' => 'create_a_book',
-  '5' => 'create_a_rental', '6' => 'list_all_rentals_for_person',
-  '7' => 'end_app'
-}.freeze
-
-def choose
-  puts "\nPlease choose an option by enter a number"
-  MENU.each { |key, element| puts "#{key} - #{element.capitalize.gsub '_', ' '}" }
-  print "\nYour option: "
-  action = MENU[gets.chomp]
-  if action
-    action
-  else
-    puts "\n(͠°◡͡°) Invalid option select a valid one\n\n"
-    choose
-  end
-end
-
 def main
   app = App.new
   puts "Welcome to School Library App ◦°˚ヽ(•◡•)ノ˚°◦\n\n"
