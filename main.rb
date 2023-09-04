e2cb1c76ef47e8087669996cf68cfedf36908f9c
Nicolas Bolañez <Zven94>
2023-09-04 16:46:20 -0300

    refactor main file

--- main.rb
+++ main.rb
@@ -1,11 +1,20 @@
 require_relative 'app'
+require_relative 'choose'
 
-def main
-  app = App.new
-  puts "Welcome to School Library App ◦°˚ヽ(•◡•)ノ˚°◦\n\n"
-  loop do
-    app.send(choose)
+# def main
+#   app = App.new
+#   puts "Welcome to School Library App ◦°˚ヽ(•◡•)ノ˚°◦\n\n"
+#   loop do
+#     app.send(choose)
+#   end
+# end
+
+# main
+
+class Main
+  def initialize
+    @choose = Choose.new
+    @app = App.new
   end
-end
 
-main
+end
